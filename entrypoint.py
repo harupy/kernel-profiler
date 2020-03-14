64c5403916f6ba0a3249b72fdfddaff5efb0f77f
harupy <hkawamura0130@gmail.com>
2020-03-15 01:49:16 +0900

    Add period

--- entrypoint.py
+++ entrypoint.py
@@ -20,7 +20,7 @@ TOP_URL = "https://www.kaggle.com"
 TIMEOUT = 15
 OUT_DIR = "output"
 HEADER = """
-## This kernel is automatically updated by [harupy/kernel-profiler](https://github.com/harupy/kernel-profiler)
+## This kernel is automatically updated by [harupy/kernel-profiler](https://github.com/harupy/kernel-profiler).
 ## Created at {}
 """
 
