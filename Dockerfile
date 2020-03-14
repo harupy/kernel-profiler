1870151e2588a12e9c5314ffeee964bb8a847c4d
harupy <hkawamura0130@gmail.com>
2020-03-14 23:13:54 +0900

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.7.6
+
+RUN apt-get update && apt-get install -y wget unzip
+
+# Install Chrome.
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add && \
+    echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | tee /etc/apt/sources.list.d/google-chrome.list && \
+    cat /etc/apt/sources.list.d/google-chrome.list && \
+    apt-get update && \
+    apt-get install -y google-chrome-stable
+
+# Install Chrome Driver.
+RUN CHROME_DRIVER_VERSION=$(curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE) && \
+    wget https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip && \
+    unzip chromedriver_linux64.zip && rm chromedriver_linux64.zip
+
+# Install Python dependencies.
+COPY ./requirements.txt .
+COPY ./requirements-dev.txt .
+RUN pip install -r requirements.txt -r requirements-dev.txt
+
+COPY entrypoint.py /entrypoint.py
+ENTRYPOINT python /entrypoint.py
---
+++ entrypoint.py
@@ -0,0 +1,277 @@
+import os
+import argparse
+import traceback
+import re
+from datetime import datetime
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from tqdm import tqdm
+
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1920x1080")
+user_agent = (
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/79.0.3945.117 Safari/537.36"
+)
+options.add_argument(f"--user-agent={user_agent}")
+driver = webdriver.Chrome("./chromedriver", options=options)
+TOP_URL = "https://www.kaggle.com"
+TIMEOUT = 15
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Kernel Profiler")
+    parser.add_argument("-c", "--comp-slug", help="competition slug (e.g. titanic)")
+    return parser.parse_args()
+
+
+def chromedriver_exists():
+    return os.path.exists("chromedriver")
+
+
+def make_soup(html):
+    return BeautifulSoup(html, "lxml")
+
+
+def get_kernel_meta(kernel):
+    return {
+        "author_name": kernel.select("span.tooltip-container")[0]
+        .get("data-tooltip")
+        .strip(),
+        "author_url": kernel.select("a.avatar")[0].get("href").strip(),
+        "thumbnail_src": kernel.select("img.avatar__thumbnail")[0].get("src").strip(),
+        "vote_count": kernel.select("span.vote-button__vote-count")[0].text.strip(),
+        "comment_count": kernel.select("a.kernel-list-item__info-block--comment")[
+            0
+        ].text.strip(),
+        "last_updated": kernel.select("div.kernel-list-item__details > span")[
+            0
+        ].text.strip(),
+        "best_score": kernel.select("div.kernel-list-item__score")[0].text.strip(),
+    }
+
+
+def utcnow():
+    return datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S (UTC)")
+
+
+def extract_public_score(html):
+    m = re.search(r'"publicScore":"(.+?)"', html)
+    return float(m.group(1)) if m else ""
+
+
+def extract_best_public_score(html):
+    m = re.search(r'"bestPublicScore":([^,]+)', html)
+    return float(m.group(1)) if m else ""
+
+
+def make_link(text, url):
+    return f"[{text}]({url})"
+
+
+def make_row(items):
+    row = "|".join(map(str, items))
+    return f"|{row}|"
+
+
+def make_table(header, data):
+    rows = []
+    rows.append(make_row(header))
+    rows.append(make_row(["-" for _ in range(len(header))]))
+    rows.extend([make_row(items) for items in data])
+    return "\n".join(rows)
+
+
+def make_thumbnail(thumbnail_src, author_name, author_url):
+    thumbnail_template = '<img src="{}" alt="{}" width="72" height="72">'
+    thumbnail = thumbnail_template.format(thumbnail_src, author_name)
+    return '<a href="{}">{}</a>'.format(author_url, thumbnail)
+
+
+def make_meta_table(meta):
+    author_link = make_link(meta["author_name"], meta["author_url"])
+    header = ["Name", "Value"]
+    meta_table = [
+        ("Author", author_link),
+        ("Best Score", meta["best_score"]),
+        ("Vote Count", meta["vote_count"]),
+        ("Comment Count", meta["comment_count"]),
+        ("Last Updated", meta["last_updated"]),
+    ]
+    return make_table(header, meta_table)
+
+
+def make_commit_table(commits):
+    commit_data = []
+
+    for commit in tqdm(commits):
+        version = commit.select("a:nth-of-type(2)")[0]
+        committed_at = commit.find("span", recursive=False).text.strip()
+        added = commit.select("span:nth-of-type(2)")[0].text.strip()
+        deleted = commit.select("span:nth-of-type(3)")[0].text.strip()
+        href = version.get("href")
+        if href is None:
+            continue
+
+        url = TOP_URL + href
+
+        # Ignore failed commits.
+        icon = commit.select("a:nth-of-type(1) > svg")[0].get("data-icon")
+        if icon == "times-circle":
+            continue
+
+        # Extract the public score.
+        resp = requests.get(url)
+        score = extract_public_score(resp.text)
+
+        if score == "":
+            continue
+
+        commit_data.append(
+            (
+                version.text.strip(),
+                score,
+                committed_at,
+                added,
+                deleted,
+                make_link("Open", url),
+            )
+        )
+
+    header = ["Version", "Score", "Committed at", "Added", "Deleted", "Link"]
+    return make_table(header, commit_data)
+
+
+def make_profile(kernel_link, commit_table, meta):
+    thumbnail = make_thumbnail(
+        meta["thumbnail_src"], meta["author_name"], meta["author_url"]
+    )
+    meta_table = make_meta_table(meta)
+    return f"""
+<br>
+
+# {kernel_link}
+
+{thumbnail}
+
+### Kernel Information
+{meta_table}
+
+### Commit History
+{commit_table}
+""".strip()
+
+
+def on_github_action():
+    return "GITHUB_ACTION" in os.environ
+
+
+def get_action_input(name):
+    return os.getenv(f"INPUT_{name.upper()}")
+
+
+def main():
+    if on_github_action():
+        comp_slug = get_action_input("slug")
+    else:
+        args = parse_args()
+        comp_slug = args.comp_slug
+
+    comp_url = f"https://www.kaggle.com/c/{comp_slug}/notebooks"
+
+    profiles = []
+    try:
+        # open the notebook page.
+        driver.get(comp_url)
+
+        # click 'Sort By' select box.
+        WebDriverWait(driver, TIMEOUT).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div.KaggleSelect"))
+        )
+        sort_by = driver.find_element_by_css_selector("div.KaggleSelect")
+        sort_by.click()
+
+        # select 'Best score'.
+        WebDriverWait(driver, TIMEOUT).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div.Select-menu-outer"))
+        )
+        options = driver.find_elements_by_css_selector("div.Select-menu-outer div")
+        best_score = [opt for opt in options if opt.text == "Best Score"][0]
+        best_score.click()
+
+        # parse kernels.
+        WebDriverWait(driver, TIMEOUT).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "a.block-link__anchor"))
+        )
+        soup = make_soup(driver.page_source)
+        kernels = soup.select("div.block-link--bordered")
+        kernels = [
+            (
+                ker.select("div.kernel-list-item__name")[0].text,  # kernel name
+                TOP_URL
+                + ker.select("a.block-link__anchor")[0].get("href"),  # kernel url
+                get_kernel_meta(ker),
+            )  # kernel metadata
+            for ker in kernels
+        ]
+        num_kernels = len(kernels)
+
+        for ker_idx, (ker_title, ker_url, ker_meta) in enumerate(kernels):
+            if ker_idx >= 2:
+                break
+            print(f"Processing {ker_url} ({ker_idx + 1} / {num_kernels})")
+
+            # Open the kernel.
+            driver.get(ker_url)
+
+            # Open the commit table.
+            WebDriverWait(driver, TIMEOUT).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "span.fa-history"))
+            )
+            commit_link = driver.find_element_by_css_selector("span.fa-history")
+            commit_link.click()
+
+            WebDriverWait(driver, TIMEOUT).until(
+                EC.presence_of_element_located(
+                    (By.CSS_SELECTOR, "div.vote-button__voters-modal-title")
+                )
+            )
+            soup = make_soup(driver.page_source)
+
+            # Process commit history.
+            pattern = r"VersionsPaneContent_IdeVersionsTable.+"
+            commits = soup.find("table", {"class": re.compile(pattern)}).select(
+                "tbody > div"
+            )
+            commit_table = make_commit_table(commits)
+            kernel_link = make_link(ker_title, ker_url)
+            profiles.append(make_profile(kernel_link, commit_table, ker_meta))
+
+        # save the result with a timestamp.
+        with open("result.md", "w") as f:
+            f.write((2 * "\n").join([f"## Created at {utcnow()}"] + profiles))
+
+    except Exception:
+        print(traceback.format_exc())
+        with open("error.html", "w") as f:
+            f.write(driver.page_source)
+    finally:
+        driver.quit()
+
+
+if __name__ == "__main__":
+    if not chromedriver_exists():
+        print(
+            "ChromeDriver not found. Please download one here: "
+            "https://chromedriver.chromium.org"
+        )
+    main()
