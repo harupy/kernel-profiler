1cf41401d4526afdb4a489a441b93ac88c66be83
harupy <hkawamura0130@gmail.com>
2020-05-03 02:56:19 +0900

    Add doctest

--- kernel_profiler/html.py
+++ kernel_profiler/html.py
@@ -1,16 +1,47 @@
 def format_attributes(attrs):
+    """
+    Examples
+    --------
+    >>> format_attributes({"a": "b", "c": "d"})
+    'a="b" c="d"'
+
+    """
     return " ".join([f'{key}="{val}"' for key, val in attrs.items()])
 
 
 def make_image_tag(attrs):
+    """
+    Examples
+    --------
+    >>> make_image_tag({"src": "https://src.com/src.png"})
+    '<img src="https://src.com/src.png">'
+
+    """
     return f"<img {format_attributes(attrs)}>"
 
 
 def make_anchor_tag(content, attrs):
+    """
+    Examples
+    --------
+    >>> make_anchor_tag("anchor", {"href": "https://href.com"})
+    '<a href="https://href.com">anchor</a>'
+
+    """
     return f"<a {format_attributes(attrs)}>{content}</a>"
 
 
 def make_thumbnail(thumbnail_src, tier_src, author_url):
+    """
+    Examples
+    --------
+    >>> make_thumbnail("thumbnail.com", "tier.com", "author.com")
+    '<a href="author.com" style="display: inline-block">\
+<img src="thumbnail.com" width="72">\
+<img src="tier.com" width="72">\
+</a>'
+
+    """
     thumbnail = make_image_tag({"src": thumbnail_src, "width": 72})
     tier = make_image_tag({"src": tier_src, "width": 72})
     return make_anchor_tag(
--- kernel_profiler/utils.py
+++ kernel_profiler/utils.py
@@ -5,6 +5,16 @@ import jupytext
 
 
 def replace_ext(path, ext):
+    """
+    Examples
+    --------
+    >>> replace_ext("foo.md", ".ipynb")
+    'foo.ipynb'
+
+    >>> replace_ext("foo.md", "ipynb")
+    'foo.ipynb'
+
+    """
     if not ext.startswith("."):
         ext = "." + ext
     root = os.path.splitext(path)[0]
@@ -12,29 +22,96 @@ def replace_ext(path, ext):
 
 
 def extract_int(text):
+    """
+    Examples
+    --------
+    >>> extract_int("ver 1")
+    '1'
+
+    >>> extract_int("ver 10")
+    '10'
+
+    >>> extract_int("ver") is None
+    True
+
+    """
     m = re.search(r"\d+", text)
     if m is not None:
         return m.group(0)
 
 
 def extract_public_score(s):
+    """
+    Examples
+    --------
+    >>> extract_public_score('"publicScore":"0.123"')
+    '0.123'
+
+    >>> extract_public_score("") is None
+    True
+
+    """
     m = re.search(r'"publicScore":"(.+?)"', s)
     if m is not None:
         return m.group(1)
 
 
 def extract_best_public_score(s):
+    """
+    Examples
+    --------
+    >>> extract_best_public_score('"bestPublicScore":0.123,')
+    '0.123'
+
+    >>> extract_best_public_score("") is None
+    True
+
+    """
     m = re.search(r'"bestPublicScore":([^,]+)', s)
     if m is not None:
         return m.group(1)
 
 
 def markdown_to_notebook(md_path, nb_path):
+    """
+    Examples
+    --------
+    >>> import os
+    >>> import tempfile
+    >>>
+    >>> with tempfile.TemporaryDirectory() as tmpdir:
+    ...     md_path = os.path.join(tmpdir, "foo.md")
+    ...     nb_path = os.path.join(tmpdir, "foo.ipynb")
+    ...
+    ...     with open(md_path, "w") as f:
+    ...         _ = f.write("# Title")
+    ...
+    ...     markdown_to_notebook(md_path, nb_path)
+    ...     os.path.exists(nb_path)
+    True
+
+    """
     notebook = jupytext.read(md_path, fmt="md")
     jupytext.write(notebook, nb_path)
 
 
 def round_run_time(run_time_str):
+    """
+    Examples
+    --------
+    >>> round_run_time("59s")
+    '59.0 s'
+
+    >>> round_run_time("60s")
+    '1.0 m'
+
+    >>> round_run_time("3599s")
+    '60.0 m'
+
+    >>> round_run_time("3600s")
+    '1.0 h'
+
+    """
     run_time = float(run_time_str[:-1])
 
     if run_time < 60:
