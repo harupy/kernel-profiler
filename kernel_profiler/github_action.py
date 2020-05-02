610832c5023e8f748fc10c81920dc5f627dfa25f
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-05-03 03:01:09 +0900

    Add doctest (#71)

--- kernel_profiler/github_action.py
+++ kernel_profiler/github_action.py
@@ -3,14 +3,49 @@ from collections import namedtuple
 
 
 def on_github_action():
+    """
+    Examples
+    --------
+    >>> import os
+    >>>
+    >>> os.environ["GITHUB_ACTION"] = "true"
+    >>> on_github_action()
+    True
+
+    """
     return "GITHUB_ACTION" in os.environ
 
 
 def get_action_input(name):
+    """
+    Examples
+    --------
+    >>> import os
+    >>>
+    >>> os.environ["INPUT_A"] = "a"
+    >>> get_action_input("a")
+    'a'
+
+    """
     return os.getenv(f"INPUT_{name.upper()}")
 
 
 def get_action_inputs(input_types):
+    """
+    Examples
+    --------
+    >>> import os
+    >>>
+    >>> os.environ["INPUT_STR"] = "a"
+    >>> os.environ["INPUT_INT"] = "0"
+    >>>
+    >>> inputs = get_action_inputs({"str": str, "int": int})
+    >>> inputs.str
+    'a'
+    >>> inputs.int
+    0
+
+    """
     Args = namedtuple("Args", list(input_types.keys()))
     return Args(*[t(get_action_input(k)) for k, t in input_types.items()])
 
--- kernel_profiler/markdown.py
+++ kernel_profiler/markdown.py
@@ -1,12 +1,38 @@
 def make_link(text, url):
+    """
+    Examples
+    --------
+    >>> make_link("foo", "https://foo.com")
+    '[foo](https://foo.com)'
+
+    """
     return f"[{text}]({url})"
 
 
 def make_row(items):
+    """
+    Examples
+    --------
+    >>> make_row(["a", "b"])
+    '|a|b|'
+
+    >>> make_row([0, 1])
+    '|0|1|'
+
+    """
     return "|".join(["", *map(str, items), ""])
 
 
 def make_table(data, headers):
+    """
+    Examples
+    --------
+    >>> print(make_table([("a", "b")], ["x", "y"]))
+    |x|y|
+    | :-- | :-- |
+    |a|b|
+
+    """
     return "\n".join(
         [make_row(headers), make_row([" :-- "] * len(headers)), *map(make_row, data)]
     )
